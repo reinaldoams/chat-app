071475d2150c58a9361517f25905e054915ac12b
Reinaldo Assis <reinaldo.assis@leanonsystems.com>
2022-07-15 12:27:08 -0300

    setting port vartiable for heroku

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-const port = 3000
+const port = process.env.port || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
